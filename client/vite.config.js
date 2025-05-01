493fd9283b826dd6ea31bc6005a429aa3c549398
maheswari301 <smaheswarieceshift@gmail.com>
2025-05-01 12:38:24 +0530

    Updated Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: `${import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL}`,
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode (development/production)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_REACT_APP_BACKEND_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
