53dc154d81e58fde29ad4b2177d0ac48fe77512d
maheswari301 <smaheswarieceshift@gmail.com>
2025-05-01 12:56:29 +0530

    Updated Vercel

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -89,7 +89,7 @@ const Register = () => {
       console.log("Submitting user data:", userData);
 
       // Make API call to register user - using proxy
-      const response = await axios.post("https://climate-eats-cuyx.vercel.app/api/users", userData);
+      const response = await axios.post(`${import.meta.env.VITE_REACT_APP_BACKEND_BASE_URL}/api/users`, userData);
 
       console.log("Server response:", response.data);
 
